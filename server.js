c5c91c2709d100dc2fc6d2dc6039f26f5ea693d7
bhavadharanik <bhavadharaniks@gmail.com>
2017-03-01 21:34:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aticle-one',function(req,res){
+app.get('/article-one',function(req,res){
    res.send('article one is requested and will be served'); 
 });
-app.get('/aticle-two',function(req,res){
+app.get('/article-two',function(req,res){
    res.send('article two is requested and will be served'); 
 });
-app.get('/aticle-three',function(req,res){
+app.get('/article-three',function(req,res){
    res.send('article three is requested and will be served'); 
 });
 app.get('/ui/style.css', function (req, res) {
